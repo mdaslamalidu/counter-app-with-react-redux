7a2d652ce270add173d5921628a2ad39069e0867
mdaslamalidu <thehometutor2022@gmail.com>
2022-12-31 16:11:53 +0600

    show project

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,31 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decreamentCounter,
+  increamentCounter,
+  resetCounter,
+} from "../services/action/actionCounter";
 
 const Counter = () => {
   const count = useSelector((state) => state.count);
-  console.log(count);
-  const handleIncreament = () => {};
+  const dispatch = useDispatch();
+  const handleIncreament = () => {
+    dispatch(increamentCounter());
+  };
+  const handleDecreament = () => {
+    dispatch(decreamentCounter());
+  };
+  const handleReset = () => {
+    dispatch(resetCounter());
+  };
   return (
     <div>
+      <h1>Welcome to React-Redux</h1>
       <h1>Counter App</h1>
       <h2>Count: {count}</h2>
       <button onClick={handleIncreament}>Increament</button>
+      <button onClick={handleDecreament}>decreament</button>
+      <button onClick={handleReset}>reset</button>
     </div>
   );
 };
